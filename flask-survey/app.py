25bab6430a58dda43cc95854e03320538885aaa1
NHower <howernickoli@gmail.com>
2022-08-15 18:10:39 -0400

    19.4 step 8 complete

--- flask-survey/app.py
+++ flask-survey/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, request, flash
+from flask import Flask, render_template, redirect, request, flash, session
 from surveys import satisfaction_survey
 from flask_debugtoolbar import DebugToolbarExtension
 
@@ -13,9 +13,16 @@ def root_route():
     """root page of the survey"""
     return render_template("root.html",survey=satisfaction_survey)
 
+@app.route("/surveystart", methods=["POST"])
+def start_survey():
+    """initialize the session's responses"""
+    session["responses"] = []
+    return redirect(f"/question/0")
+
 @app.route("/question/<int:num>")
 def question_route(num):
-    """route to question pages for the survey"""
+    """Individual questions of the survey"""
+    survey_resps = session.get("responses",[])
     if len(survey_resps) < len(satisfaction_survey.questions):
         if num == len(survey_resps):
             question = satisfaction_survey.questions[num]
@@ -28,11 +35,13 @@ def question_route(num):
 
 @app.route("/answer", methods=["POST"])
 def answer_route():
+    """add the answer to the responses"""
+    survey_resps = session.get("responses",[])
     survey_resps.append(request.form["choice"])
+    session["responses"] = survey_resps
     return redirect(f"/question/{len(survey_resps)}")
 
 @app.route("/thanks")
 def thanks_route():
-    """route to the thank you page once a survey is completed"""
+    """Thank you page once a survey is completed"""
     return render_template("thanks.html")
-    

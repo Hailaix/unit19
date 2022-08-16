3f6412608c0880f7580c6e8040480737fb326802
NHower <howernickoli@gmail.com>
2022-08-15 20:03:03 -0400

    19.4 further study up to four

--- flask-survey/app.py
+++ flask-survey/app.py
@@ -1,17 +1,26 @@
 from flask import Flask, render_template, redirect, request, flash, session
-from surveys import satisfaction_survey
+from surveys import satisfaction_survey, personality_quiz, surveys
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 debug = DebugToolbarExtension(app)
 
-survey_resps = []
 
 @app.route("/")
 def root_route():
-    """root page of the survey"""
-    return render_template("root.html",survey=satisfaction_survey)
+    """survey select page"""
+    return render_template("surveyselect.html",s=surveys)
+
+@app.route("/survey/<select>")
+def survey_selected(select):
+    """description page of the selected survey"""
+    if surveys.get(select):
+        session["survey"] = select
+        return render_template("root.html",survey=surveys[session["survey"]])
+    else:
+        return redirect("/")
+    # return render_template("root.html",survey=satisfaction_survey)
 
 @app.route("/surveystart", methods=["POST"])
 def start_survey():
@@ -23,9 +32,9 @@ def start_survey():
 def question_route(num):
     """Individual questions of the survey"""
     survey_resps = session.get("responses",[])
-    if len(survey_resps) < len(satisfaction_survey.questions):
+    if len(survey_resps) < len(surveys[session["survey"]].questions):
         if num == len(survey_resps):
-            question = satisfaction_survey.questions[num]
+            question = surveys[session["survey"]].questions[num]
             return render_template("question.html",q=question,i=num)
         else:
             flash("Invalid Question", "error")
@@ -37,11 +46,12 @@ def question_route(num):
 def answer_route():
     """add the answer to the responses"""
     survey_resps = session.get("responses",[])
-    survey_resps.append(request.form["choice"])
+    res_tup = (request.form["choice"],request.form.get("comment",""))
+    survey_resps.append(res_tup)
     session["responses"] = survey_resps
     return redirect(f"/question/{len(survey_resps)}")
 
 @app.route("/thanks")
 def thanks_route():
     """Thank you page once a survey is completed"""
-    return render_template("thanks.html")
+    return render_template("thanks.html",s=surveys[session["survey"]])
